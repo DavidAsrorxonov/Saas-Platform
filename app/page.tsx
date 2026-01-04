9c3382de316a7f667fbbca16517229e21419107e
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2026-01-04 11:57:48 +0900

    add Home component with CompanionCard rendering and update layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
+import CompanionCard from "@/components/CompanionCard";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Welcome to my SaaS App</h1>
-      <Button>Get Started</Button>
-      hello
-    </div>
+    <main>
+      <h1 className="text-2xl underline">Popular Companions</h1>
+      <section className="flex gap-4 justify-between items-start w-full max-lg:flex-col-reverse max-lg:items-center">
+        <CompanionCard />
+        <CompanionCard />
+        <CompanionCard />
+      </section>
+    </main>
   );
 }
