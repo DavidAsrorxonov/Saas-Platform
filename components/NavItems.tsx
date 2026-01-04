4707f693784d95af80a979f9ec5f4f395d3c6d5e
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2026-01-04 11:51:53 +0900

    implement NavItems component to render navigation links dynamically

--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -1,5 +1,16 @@
+import { navItems } from "@/constants/navItems";
+import Link from "next/link";
+
 const NavItems = () => {
-  return <nav className="flex items-center gap-4"></nav>;
+  return (
+    <nav className="flex items-center gap-4">
+      {navItems.map(({ label, href }) => (
+        <Link href={href} key={label}>
+          {label}
+        </Link>
+      ))}
+    </nav>
+  );
 };
 
 export default NavItems;
