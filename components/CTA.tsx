630ab7387624443b64b5b19aafdf8df14a8febc7
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2026-01-04 22:18:22 +0900

    feat: implement CTA component with card layout and link to create new companion

--- components/CTA.tsx
+++ components/CTA.tsx
@@ -1,7 +1,31 @@
 import React from "react";
+import { Card, CardHeader, CardTitle } from "./ui/card";
+import { Badge } from "./ui/badge";
+import Image from "next/image";
+import { Button } from "./ui/button";
+import { Plus } from "lucide-react";
+import Link from "next/link";
 
 const CTA = () => {
-  return <div>CTA</div>;
+  return (
+    <Card className="rounded-none px-7 py-10 flex flex-col items-center text-center gap-5 w-1/3 max-lg:w-1/2 max-md:w-full">
+      <Badge>Start learning your way.</Badge>
+      <h2 className="text-3xl font-bold">
+        Build and personalize learning companion
+      </h2>
+      <p>
+        Pick a name, subject, voice, & personalize - and start learning through
+        conversations that feel natural and fun.
+      </p>
+      <Image src={"/images/cta.svg"} alt="cta" width={362} height={232} />
+      <Button className="w-full">
+        <Plus />
+        <Link href={"/companions/new"}>
+          <p>Build a new companion</p>
+        </Link>
+      </Button>
+    </Card>
+  );
 };
 
 export default CTA;
