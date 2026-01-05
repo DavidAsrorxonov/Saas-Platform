03c1294e5a7048db37d501166a992a0d335c27aa
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2026-01-05 16:05:09 +0900

    feat: implement CompanionForm component and integrate it into NewCompanion page

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,5 +1,14 @@
+import CompanionForm from "@/components/CompanionForm";
+
 const NewCompanion = () => {
-  return <div>NewCompanion</div>;
+  return (
+    <main className="lg:w-1/3 md:w-2/3 items-center justify-center mx-auto px-14 flex flex-col gap-8 bg-background h-full max-w-350 pt-10 max-sm:px-2 mb-5">
+      <article className="w-full gap-4 flex flex-col">
+        <h1 className="text-3xl font-bold">Companion Builder</h1>
+        <CompanionForm />
+      </article>
+    </main>
+  );
 };
 
 export default NewCompanion;
