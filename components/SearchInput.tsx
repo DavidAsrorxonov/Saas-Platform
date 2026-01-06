78ae72be9a5da3a49405e647335032a74b800025
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2026-01-06 14:42:08 +0900

    feat: implement SearchInput component with input group for companion search functionality

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -1,7 +1,32 @@
-import React from "react";
+"use client";
+
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useState } from "react";
+import { InputGroup, InputGroupAddon, InputGroupInput } from "./ui/input-group";
+import { Search } from "lucide-react";
 
 const SearchInput = () => {
-  return <div>SearchInput</div>;
+  const pathname = usePathname();
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const query = searchParams.get("topic") || "";
+
+  const [searchQuery, setSearchQuery] = useState("");
+
+  return (
+    <div className="relative items-center flex gap-2 px-2 py-1 h-fit">
+      <InputGroup>
+        <InputGroupInput
+          placeholder="Search companions..."
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <InputGroupAddon>
+          <Search />
+        </InputGroupAddon>
+      </InputGroup>
+    </div>
+  );
 };
 
 export default SearchInput;
