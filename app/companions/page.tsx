c46df2bff70560bdac067ab26360c0f3bbb31a46
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2026-01-06 14:31:27 +0900

    feat: enhance CompanionsPage to fetch and display companions with filtering options

--- app/companions/page.tsx
+++ app/companions/page.tsx
@@ -1,5 +1,36 @@
-const CompanionsPage = () => {
-  return <div>CompanionsPage</div>;
+import CompanionCard from "@/components/CompanionCard";
+import { fetchCompanions } from "@/lib/actions/companion.actions";
+import { SearchParams } from "@/types/searchParams";
+
+const CompanionsPage = async ({
+  searchParams,
+}: {
+  searchParams: Promise<SearchParams>;
+}) => {
+  const params = await searchParams;
+
+  const subject = params.subject ?? "";
+  const topic = params.topic ?? "";
+
+  console.log("Params", { subject, topic });
+
+  const companions = await fetchCompanions({ subject, topic });
+
+  console.log(companions);
+
+  return (
+    <main className="flex items-center justify-center mx-auto px-14 flex-col gap-8 bg-background h-full max-w-350 pt-10 max-sm:px-2 mb-5">
+      <section className="flex justify-between gap-4 max-sm:flex-col">
+        <h1 className="text-3xl font-bold">Companion Library</h1>
+        <div className="flex gap-4">Filters</div>
+      </section>
+      <section className="flex flex-wrap gap-4 w-full max-md:justify-center justify-between">
+        {companions.map((companion) => (
+          <CompanionCard key={companion.id} {...companion} />
+        ))}
+      </section>
+    </main>
+  );
 };
 
 export default CompanionsPage;
---
+++ types/searchParams.ts
@@ -0,0 +1,4 @@
+export type SearchParams = {
+  subject?: string;
+  topic?: string;
+};
