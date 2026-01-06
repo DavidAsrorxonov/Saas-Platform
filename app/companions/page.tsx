ca33d2e6491bc02afb334899c228b7bbb17f8b47
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2026-01-06 14:33:16 +0900

    feat: add search and subject filter components to CompanionsPage

--- app/companions/page.tsx
+++ app/companions/page.tsx
@@ -1,4 +1,6 @@
 import CompanionCard from "@/components/CompanionCard";
+import SearchInput from "@/components/SearchInput";
+import SubjectFilter from "@/components/SubjectFilter";
 import { fetchCompanions } from "@/lib/actions/companion.actions";
 import { SearchParams } from "@/types/searchParams";
 
@@ -22,7 +24,10 @@ const CompanionsPage = async ({
     <main className="flex items-center justify-center mx-auto px-14 flex-col gap-8 bg-background h-full max-w-350 pt-10 max-sm:px-2 mb-5">
       <section className="flex justify-between gap-4 max-sm:flex-col">
         <h1 className="text-3xl font-bold">Companion Library</h1>
-        <div className="flex gap-4">Filters</div>
+        <div className="flex gap-4">
+          <SearchInput />
+          <SubjectFilter />
+        </div>
       </section>
       <section className="flex flex-wrap gap-4 w-full max-md:justify-center justify-between">
         {companions.map((companion) => (
