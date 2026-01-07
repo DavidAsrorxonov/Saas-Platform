393ea1a4e1ea0f9d191c3e6bf63798c34a411f8f
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2026-01-07 19:03:09 +0900

    feat: enhance SearchInput component with debounced URL updates for search functionality

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -1,9 +1,11 @@
 "use client";
 
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { InputGroup, InputGroupAddon, InputGroupInput } from "./ui/input-group";
 import { Search } from "lucide-react";
+
+import { formUrlQuery, removeKeysFromUrlQuery } from "@jsmastery/utils";
 
 const SearchInput = () => {
   const pathname = usePathname();
@@ -12,6 +14,29 @@ const SearchInput = () => {
   const query = searchParams.get("topic") || "";
 
   const [searchQuery, setSearchQuery] = useState("");
+
+  useEffect(() => {
+    const delayDebounceFn = setTimeout(() => {
+      if (searchQuery) {
+        const newUrl = formUrlQuery({
+          params: searchParams.toString(),
+          key: "topic",
+          value: searchQuery,
+        });
+
+        router.push(newUrl, { scroll: false });
+      } else {
+        if (pathname === "/companions") {
+          const newUrl = removeKeysFromUrlQuery({
+            params: searchParams.toString(),
+            keysToRemove: ["topic"],
+          });
+
+          router.push(newUrl, { scroll: false });
+        }
+      }
+    }, 500);
+  }, [searchQuery, searchParams, router, pathname]);
 
   return (
     <div className="relative items-center flex gap-2 px-2 py-1 h-fit">
