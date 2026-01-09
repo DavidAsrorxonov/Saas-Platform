9a4796ac24f955d785accd6bf81e3dfd50a862c0
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2026-01-09 23:42:29 +0900

    install shadcn avatar ui component; replace manually written UI with shadcn's components; create, import and use Companion Component;

--- app/companions/[id]/page.tsx
+++ app/companions/[id]/page.tsx
@@ -1,9 +1,10 @@
+import CompanionComponent from "@/components/CompanionComponent";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Badge } from "@/components/ui/badge";
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
 import { fetchCompanion } from "@/lib/actions/companion.actions";
 import { currentUser } from "@clerk/nextjs/server";
-import Image from "next/image";
 import { redirect } from "next/navigation";
-import React from "react";
 
 const CompanionSession = async ({ params }: CompanionSessionPageProps) => {
   const { id } = await params;
@@ -15,32 +16,33 @@ const CompanionSession = async ({ params }: CompanionSessionPageProps) => {
   if (!companion) redirect("/companions");
 
   return (
-    <main className="mx-auto px-14 flex flex-col gap-8 bg-background h-full max-w-[1400px] pt-10 max-sm:px-2 mb-5">
-      <article className="flex justify-between p-6 max-md:flex-col">
-        <div className="flex items-center gap-2">
-          <div className="size-18 flex items-center justify-center max-md:hidden">
-            <Image
-              src={`/icons/${companion.subject}.svg`}
-              alt="companion"
-              width={35}
-              height={35}
-            />
-          </div>
+    <main className="mx-auto px-14 flex flex-col gap-8 bg-background h-full max-w-350 pt-10 max-sm:px-2 mb-5">
+      <Card>
+        <CardHeader className="flex flex-row items-center justify-between">
+          <div className="flex items-center gap-4">
+            <Avatar className="size-12 bg-primary">
+              <AvatarImage
+                src={`/icons/${companion.subject}.svg`}
+                className="p-3"
+              />
+            </Avatar>
 
-          <div className="flex flex-col gap-2">
-            <div className="flex items-center gap-2">
-              <p className="font-bold text-2xl">{companion.name}</p>
-              <Badge>{companion.subject}</Badge>
+            <div>
+              <CardTitle className="flex items-center gap-2">
+                {companion.name}
+                <Badge>{companion.subject}</Badge>
+              </CardTitle>
+              <p className="text-muted-foreground text-sm">{companion.topic}</p>
             </div>
-
-            <p className="text-lg">{companion.topic}</p>
           </div>
-        </div>
 
-        <div className="items-start text-2xl max-md:hidden">
-          {companion.duration} minutes
-        </div>
-      </article>
+          <p className="hidden md:block text-sm text-muted-foreground">
+            {companion.duration} minutes
+          </p>
+        </CardHeader>
+      </Card>
+
+      <CompanionComponent />
     </main>
   );
 };
