7f523ee7b64a1b55c8b67d7715e540aad0ac8e14
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2026-01-10 20:07:50 +0900

    create an enum for call statuses;

--- components/CompanionComponent.tsx
+++ components/CompanionComponent.tsx
@@ -1,4 +1,8 @@
-import React from "react";
+"use client";
+
+import { cn } from "@/lib/utils";
+import { CallStatus } from "@/types/callStatus";
+import { useState } from "react";
 
 const CompanionComponent = ({
   companionId,
@@ -10,11 +14,17 @@ const CompanionComponent = ({
   style,
   voice,
 }: CompanionComponentProps) => {
+  const [callStatus, setcallStatus] = useState<CallStatus>(CallStatus.INACTIVE);
+
   return (
     <section className="flex flex-col h-[70vh]">
       <section className="flex gap-8 max-sm:flex-col">
         <div className="w-2/3 max-sm:w-full flex flex-col gap-4 justify-center items-center border">
-          <div className="size-75 flex items-center justify-center max-sm:size-25 mt-4 bg-primary"></div>
+          <div className="size-75 flex items-center justify-center max-sm:size-25 mt-4 bg-primary">
+            <div
+              className={cn("absolute transition-opacity duration-1000")}
+            ></div>
+          </div>
         </div>
       </section>
     </section>
