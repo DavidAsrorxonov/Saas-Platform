4f693541e2a8ad40f9f53d4c49caf071fcc77479
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2026-01-10 16:16:16 +0900

    modify UI; add voiceanim json lottie for talking UI

--- components/CompanionComponent.tsx
+++ components/CompanionComponent.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const CompanionComponent = () => {
-  return <div>CompanionComponent</div>;
+  return (
+    <section className="flex flex-col h-[70vh]">
+      <section className="flex gap-8 max-sm:flex-col">
+        <div className="w-2/3 max-sm:w-full flex flex-col gap-4 justify-center items-center border">
+          <div className="size-75 flex items-center justify-center max-sm:size-25 mt-4 bg-primary"></div>
+        </div>
+      </section>
+    </section>
+  );
 };
 
 export default CompanionComponent;
